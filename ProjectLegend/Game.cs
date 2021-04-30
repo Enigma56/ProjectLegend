e4e0ca3e96a62a0a94f53e2b1867eaefc21985b4
Enigma56 <confuciussays58@gmail.com>
2021-04-30 00:45:28 -0400

    Creation of ProjectLegend

---
+++ ProjectLegend/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectLegend
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the main gameloop!");
+        }
+    }
+}
