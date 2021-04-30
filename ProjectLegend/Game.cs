533586ab977273a11d11a81e00bd29124b77ecdd
Enigma56 <confuciussays58@gmail.com>
2021-04-30 00:47:49 -0400

    Test

--- ProjectLegend/Game.cs
+++ ProjectLegend/Game.cs
@@ -7,6 +7,7 @@ namespace ProjectLegend
         static void Main(string[] args)
         {
             Console.WriteLine("This is the main gameloop!");
+            Console.WriteLine("Spaghetti!");
         }
     }
 }
